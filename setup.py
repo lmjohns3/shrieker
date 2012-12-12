ce8a4549cba8a77b78b0911036b8ce98770d2e1b
Leif Johnson <leif@leifjohnson.net>
2012-12-11 18:07:53 -0600

    Add README.md to package manifest ; update setup.py to reflect best packaging know-how to date.

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
+import os
 import setuptools
 
 setuptools.setup(
     name='lmj.nethack',
-    version='0.1',
+    version='0.1.1',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
     author='Leif Johnson',
     author_email='leif@leifjohnson.net',
-    description='A pty wrapper over nethack',
-    long_description=open('README.md').read(),
+    description='A pty wrapper for nethack',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
     license='MIT',
+    url='http://github.com/lmjohns3/py-nethack',
     keywords=('nethack '
               ),
-    url='http://github.com/lmjohns3/py-nethack',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
